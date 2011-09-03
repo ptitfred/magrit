82fb2ea2335954f55298f4cb940fe34f0d23bb20
Frédéric Menou <frederic.menou@gmail.com>
2011-09-04 01:03:22 +0200

    server: minor - logs in DummyBuildStatusesService

--- server/magrit-server/src/main/java/org/kercoin/magrit/services/DummyBuildStatusesService.java
+++ server/magrit-server/src/main/java/org/kercoin/magrit/services/DummyBuildStatusesService.java
@@ -1,11 +1,16 @@
 package org.kercoin.magrit.services;
 
 import org.eclipse.jgit.lib.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DummyBuildStatusesService implements BuildStatusesService {
 
+	protected final Logger log = LoggerFactory.getLogger(getClass());
+
 	@Override
 	public BuildStatus getStatus(Repository repository, String sha1) {
+		log.info("Checking status for {} @ {}", repository.getDirectory(), sha1);
 		return BuildStatus.FAILED;
 	}
 
