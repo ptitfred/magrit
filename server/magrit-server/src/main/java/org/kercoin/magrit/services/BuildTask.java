e482828db11f47dbd2999778385e84d6a8233fdc
Frédéric Menou <frederic.menou@gmail.com>
2011-09-05 23:41:31 +0200

    server: lecture du script depuis le fichier .magrit

--- server/magrit-server/src/main/java/org/kercoin/magrit/services/BuildTask.java
+++ server/magrit-server/src/main/java/org/kercoin/magrit/services/BuildTask.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.concurrent.Callable;
 
@@ -13,7 +14,12 @@ import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.dircache.DirCacheEntry;
+import org.eclipse.jgit.errors.AmbiguousObjectException;
 import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.errors.NoWorkTreeException;
+import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -153,9 +159,12 @@ public class BuildTask implements Callable<BuildResult> {
 		return new File(repository.getDirectory(), "MAGRIT_BUILD");
 	}
 	
-	String findCommand() {
-		// TODO find the build command in the repository;
-		return "mvn -f server/magrit-server/pom.xml clean install";
+	String findCommand() throws AmbiguousObjectException, IOException {
+		ObjectId ref = repository.resolve(this.target.getU() + ":" + ".magrit");
+		if (ref != null) {
+			return new String(repository.getObjectDatabase().newReader().open(ref).getBytes(), "UTF-8");
+		}
+		return null;
 	}
 	
 }
