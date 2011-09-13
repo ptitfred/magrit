ed3f7ff08e8621cf5481905c34f3399c477f6149
Frédéric Menou <frederic.menou@gmail.com>
2011-09-13 02:32:20 +0200

    Utilities: check a revstr is a full-SHA1

--- server/magrit-server/src/main/java/org/kercoin/magrit/utils/GitUtils.java
+++ server/magrit-server/src/main/java/org/kercoin/magrit/utils/GitUtils.java
@@ -2,6 +2,7 @@ package org.kercoin.magrit.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import org.eclipse.jgit.errors.AmbiguousObjectException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
@@ -53,5 +54,12 @@ public class GitUtils {
 			return null;
 		}
 		return new String(bytes, "UTF-8");
+	}
+	
+	public boolean isSha1(String candidate) {
+		if (candidate.length()!=40) {
+			return false;
+		}
+		return Pattern.compile("[0-9a-z]{40}").matcher(candidate).matches();
 	}
 }
