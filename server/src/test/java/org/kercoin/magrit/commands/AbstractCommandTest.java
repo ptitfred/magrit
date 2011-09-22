3af3741ad31164214c6f1e7d0b0bd60bfe19c059
Frédéric Menou <frederic.menou@gmail.com>
2011-09-23 01:39:13 +0200

    quality: test coverage on AbstractCommand => fix inside!
    There was a small leak in SHA1 validation.
    Not that bad because clients are friendly (git rev-parse from
    official git packages compute valid 40bytes hexa coded SHA1)

---
+++ server/src/test/java/org/kercoin/magrit/commands/AbstractCommandTest.java
@@ -0,0 +1,132 @@
+package org.kercoin.magrit.commands;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.util.io.NullOutputStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kercoin.magrit.Context;
+import org.kercoin.magrit.utils.GitUtils;
+import org.kercoin.magrit.utils.LoggerInputStream;
+import org.kercoin.magrit.utils.LoggerOutputStream;
+import org.mockito.Answers;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AbstractCommandTest {
+
+	AbstractCommand<?> cmd;
+	
+	@Mock(answer=Answers.RETURNS_DEEP_STUBS)
+	Context ctx;
+	
+	static class FakeCommand extends AbstractCommand<FakeCommand> {
+		public FakeCommand(Context ctx) { super(ctx); }
+		public void run() {}
+		public FakeCommand command(String command) throws Exception { return null; }
+		protected Class<FakeCommand> getType() { return null; }
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		cmd = new FakeCommand(new Context(new GitUtils()));
+	}
+
+	@Test
+	public void testSetInputStream() throws IOException {
+		// given
+		cmd.logStreams = true;
+		// when
+		cmd.setInputStream(new ByteArrayInputStream(new byte[]{6, '*', 9, '=', 42}));
+		// then
+		assertThat(cmd.getInputStream()).isInstanceOf(LoggerInputStream.class);
+		cmd.getInputStream().read(new byte[4]);
+		assertThat(cmd.getInputStream().read()).isEqualTo(42);
+	}
+
+	@Test
+	public void testSetOutputStream() {
+		// given ---------------------------------
+		cmd.logStreams = true;
+		
+		// when ----------------------------------
+		cmd.setOutputStream(NullOutputStream.INSTANCE);
+		
+		// then ----------------------------------
+		assertThat(cmd.getOutputStream()).isInstanceOf(LoggerOutputStream.class);
+	}
+
+	@Test
+	public void testSetErrorStream() {
+		// given ---------------------------------
+		cmd.logStreams = true;
+		
+		// when ----------------------------------
+		cmd.setErrorStream(NullOutputStream.INSTANCE);
+		
+		// then ----------------------------------
+		assertThat(cmd.getErrorStream()).isInstanceOf(LoggerOutputStream.class);
+	}
+
+	@Test
+	public void testCheckSha1_valid() {
+		cmd.checkSha1("012340abcdef2340123401234012340123401234");
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testCheckSha1_invalid() {
+		cmd.checkSha1("01234012340gh340123401234012340123401234");
+	}
+
+	@Test
+	public void testCreateRepository_default() throws IOException {
+		// given ---------------------------------
+		given(ctx.configuration().getRepositoriesHomeDir()).willReturn(new File("/tmp"));
+
+		// when ----------------------------------
+		new FakeCommand(ctx).createRepository("/r1");
+
+		// then ----------------------------------
+		ArgumentCaptor<File> where = ArgumentCaptor.forClass(File.class);
+		verify(ctx.getGitUtils()).createRepository(where.capture());
+		assertThat(where.getValue().getAbsolutePath()).isEqualTo("/tmp/r1");
+	}
+
+	@Test
+	public void testCreateRepository_nominal() throws IOException {
+		// given ---------------------------------
+		given(ctx.configuration().getRepositoriesHomeDir()).willReturn(new File("/tmp"));
+
+		// when ----------------------------------
+		new FakeCommand(ctx).createRepository("r1");
+
+		// then ----------------------------------
+		ArgumentCaptor<File> where = ArgumentCaptor.forClass(File.class);
+		verify(ctx.getGitUtils()).createRepository(where.capture());
+		assertThat(where.getValue().getAbsolutePath()).isEqualTo("/tmp/r1");
+	}
+
+	@Test
+	public void testCreateRepository_absurd() throws IOException {
+		// given ---------------------------------
+		given(ctx.configuration().getRepositoriesHomeDir()).willReturn(new File("/tmp"));
+
+		// when ----------------------------------
+		new FakeCommand(ctx).createRepository("///r1");
+
+		// then ----------------------------------
+		ArgumentCaptor<File> where = ArgumentCaptor.forClass(File.class);
+		verify(ctx.getGitUtils()).createRepository(where.capture());
+		assertThat(where.getValue().getAbsolutePath()).isEqualTo("/tmp/r1");
+	}
+
+}
