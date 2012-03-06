38583c988a9f5bd6a5afda778c6205cac9948b37
Dani Perez <danielperezalcazar@gmail.com>
2012-03-06 23:05:24 +0100

    CMake enabled.
    Windows 32 bits cross-compilation in Fedora & Linux 32 bit compilation
    should be working.

---
+++ client/src/main/c++/README.md
@@ -0,0 +1,21 @@
+Compiling for Linux target 
+==========================
+
+ > make build
+ > cd build
+ > cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain/Linux-gcc-i686.cmake ../
+
+
+Cross-compiling for Windows target
+==================================
+
+Check first if toolchain/Windows-mingw-i686.cmake supports your distribution
+If it's supported, congratulations! You can then do:
+
+ > make build
+ > cd build
+ > cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain/Windows-mingw-i686.cmake ../
+
+If it's not, enabling support for your distro shouldn't be very difficult
+(have a look at the first lines of the toolchain file).
+
---
+++ client/src/main/c++/toolchain/Windows-mingw-i686.cmake
@@ -0,0 +1,26 @@
+#
+# NOTE: These are distro-dependent. Add your favorite
+#       distro check & config parameters here.
+#
+IF ( EXISTS "/etc/fedora-release" )
+    SET ( MINGW_PREFIX   i686-pc-mingw32 )
+    SET ( Boost_COMPILER -gcc45          )
+ELSE ()
+    message ( SEND_ERROR "Unknown host platform" )
+ENDIF()
+
+include(CMakeForceCompiler)
+
+SET ( CMAKE_SYSTEM_NAME Windows )
+
+ADD_DEFINITIONS ( "-D__CYGWIN__" )
+
+SET ( CMAKE_C_COMPILER ${MINGW_PREFIX}-gcc )
+SET ( CMAKE_FORCE_CXX_COMPILER ${MINGW_PREFIX}-g++ GNU )
+SET ( CMAKE_RC_COMPILER ${MINGW_PREFIX}-windres )
+
+SET ( CMAKE_FIND_ROOT_PATH /usr/${MINGW_PREFIX}/sys-root/mingw/ )
+
+SET ( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+SET ( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+SET ( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
