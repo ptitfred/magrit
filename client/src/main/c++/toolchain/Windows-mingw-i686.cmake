98948b0494ada5fb39b476186983d0824358b40f
Dani Perez <danielperezalcazar@gmail.com>
2012-03-22 21:09:55 +0100

    [travis test] trying to fix ubuntu config

--- client/src/main/c++/toolchain/Windows-mingw-i686.cmake
+++ client/src/main/c++/toolchain/Windows-mingw-i686.cmake
@@ -3,10 +3,12 @@
 #       distro check & config parameters here.
 #
 IF ( EXISTS "/etc/fedora-release" )
-    SET ( MINGW_PREFIX   /usr/i686-pc-mingw32/sys-root/mingw/ )
+    SET ( MINGW_PREFIX   "i686-pc-mingw32" )
+    SET ( MINGW_SYSROOT  "/usr/${MINGW_PREFIX}/sys-root/mingw/" )
     SET ( Boost_COMPILER -gcc45 )
 ELSEIF ( EXISTS "/etc/debian_version" )
-    SET ( MINGW_PREFIX   /usr/i586-mingw32msvc/ )
+    SET ( MINGW_PREFIX   "i586-mingw32msvc"     )
+    SET ( MINGW_SYSROOT  "/usr/${MINGW_PREFIX}" )
 ELSE ()
     message ( FATAL_ERROR "Unknown host platform" )
 ENDIF()
@@ -21,7 +23,7 @@ SET ( CMAKE_C_COMPILER ${MINGW_PREFIX}-gcc )
 SET ( CMAKE_FORCE_CXX_COMPILER ${MINGW_PREFIX}-g++ GNU )
 SET ( CMAKE_RC_COMPILER ${MINGW_PREFIX}-windres )
 
-SET ( CMAKE_FIND_ROOT_PATH ${MINGW_PREFIX} )
+SET ( CMAKE_FIND_ROOT_PATH ${MINGW_SYSROOT} )
 
 SET ( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
 SET ( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
