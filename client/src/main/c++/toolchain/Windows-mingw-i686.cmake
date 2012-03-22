1dfe4779c5b6265afc5a5d6b407dd81562e8987b
Dani Perez <danielperezalcazar@gmail.com>
2012-03-22 20:57:45 +0100

    [travis test] ubuntu support added

--- client/src/main/c++/toolchain/Windows-mingw-i686.cmake
+++ client/src/main/c++/toolchain/Windows-mingw-i686.cmake
@@ -3,8 +3,10 @@
 #       distro check & config parameters here.
 #
 IF ( EXISTS "/etc/fedora-release" )
-    SET ( MINGW_PREFIX   i686-pc-mingw32 )
-    SET ( Boost_COMPILER -gcc45          )
+    SET ( MINGW_PREFIX   /usr/i686-pc-mingw32/sys-root/mingw/ )
+    SET ( Boost_COMPILER -gcc45 )
+ELSEIF ( EXISTS "/etc/debian_version" )
+    SET ( MINGW_PREFIX   /usr/i586-mingw32msvc/ )
 ELSE ()
     message ( FATAL_ERROR "Unknown host platform" )
 ENDIF()
@@ -19,7 +21,7 @@ SET ( CMAKE_C_COMPILER ${MINGW_PREFIX}-gcc )
 SET ( CMAKE_FORCE_CXX_COMPILER ${MINGW_PREFIX}-g++ GNU )
 SET ( CMAKE_RC_COMPILER ${MINGW_PREFIX}-windres )
 
-SET ( CMAKE_FIND_ROOT_PATH /usr/${MINGW_PREFIX}/sys-root/mingw/ )
+SET ( CMAKE_FIND_ROOT_PATH ${MINGW_PREFIX} )
 
 SET ( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
 SET ( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
